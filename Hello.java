2e21490293fb056b29342f1a9d90adf5515433dc
lodimk2 <69815640+lodimk2@users.noreply.github.com>
2020-08-17 14:11:20 -0400

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,24 @@
+/**********************************************************************
+*Hello
+************************************************************************Musaddiq Lodi 
+* 8/17/2020
+* CMSC 255 Section 090
+**********************************************************************/
+
+public class Hello {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Musaddiq Lodi");
+		System.out.println("Hello");
+		System.out.println("Printing text that says introductory information");
+		System.out.println("JDK 8");
+		System.out.println("CMSC 255 Section 090");
+		
+
+
+
+	}
+
+
+}
